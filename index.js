80b02fbb574baa2c3762379305f1e2cbf4330e84
Ibrahim <bacing88@gmail.com>
2021-09-11 05:44:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,47 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+const express = require("express");
+const bodyParser = require("body-parser");
+const multer = require("multer");
+const cors = require("cors");
+const path = require("path");
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+app.use(cors());
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/uploads");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      path.parse(file.originalname).name +
+        "-" +
+        Date.now() +
+        path.extname(file.originalname)
+    );
+  },
+});
+
+const upload = multer({ storage: storage });
+
+app.post(`/api/upload`, upload.single("photo"), (req, res) => {
+  // save filename nya ke database
+  // return url ke user
+
+  let finalImageURL =
+    req.protocol + "://" + req.get("host") + "/uploads/" + req.file.filename;
+
+  res.json({ image: finalImageURL });
+});
+
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, (e) => {
+  if (e) throw e;
+
+  console.log(`Server is running on PORT : ${PORT}`);
+});
